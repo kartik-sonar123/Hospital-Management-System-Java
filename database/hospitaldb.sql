8bf07a6c09361529a2e1ef91e54a200fc50e1d44
Kartik Sonar <159419515+kartik-sonar123@users.noreply.github.com>
2025-02-01 23:37:47 +0530

    Update hospitaldb.sql

--- database/hospitaldb.sql
+++ database/hospitaldb.sql
@@ -2,7 +2,7 @@ CREATE DATABASE hospital;
 USE hospital;
 CREATE TABLE patients
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     age INT NOT NULL,
     gender VARCHAR(10) NOT NULL
@@ -10,21 +10,20 @@ CREATE TABLE patients
 
 CREATE TABLE doctors
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     specialization VARCHAR(255) NOT NULL
 );
 
-INSERT INTO doctors(id,name,specialization)
-VALUES
-	(1,'Dr.Altaf_patel','Cardiology'),
+INSERT INTO doctors(id,name,specialization) VALUES
+    (1,'Dr.Altaf_patel','Cardiology'),
     (2,'Dr.Lalit_patil','Neurology'),
     (3,'Dr.Jain','Orthopedics'),
     (4,'Dr.Khushi','Dermatology');
     
 CREATE TABLE appointments
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     patient_id INT NOT NULL,
     doctor_id INT NOT NULL,
     appointment_date DATE NOT NULL,
